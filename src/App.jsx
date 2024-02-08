61e40c14def596a78d78ca085fd79b8b855d729f
ShineHtetWai29 <familyshien293@gmail.com>
2024-02-08 14:21:18 +0630

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Mainvisual from './pages/mainvisualPage/Mainvisual'
 
 function App() {
 
   return (
     <>
-      
+      <Mainvisual/>
     </>
   )
 }
---
+++ src/pages/mainvisualPage/Mainvisual.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Mainvisual.css'
+const Mainvisual = () => {
+  return (
+    <div className='mainvisual'>Mainvisual</div>
+  )
+}
+
+export default Mainvisual
