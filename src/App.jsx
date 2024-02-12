5f027eb4880bb10881cfe9978960d707f150a3f2
Kokhantkyaw <khantkyaw281@gmail.com>
2024-02-12 16:15:57 +0630

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import Mainvisual from './pages/mainvisualPage/Mainvisual'
+import Login from './pages/loginPage/Login'
+import Signup from './pages/signupPage/Signup'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Mainvisual/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
+    
+    
+      {/* <Mainvisual/> */}
     </>
   )
 }
--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FaFacebook } from "react-icons/fa"
+import { FaSquareTwitter } from "react-icons/fa6"
+import { FaGooglePlus } from "react-icons/fa"
+import './Login.css'
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div>
-      
+    <div id='outerDiv'>
+      <div id='innerDiv'>
+          <p id='myTitle'>LOGIN INTO YOUR ACCOUNT</p>
+          <p id='mySubtitle'>Login using Social Networks</p>
+          <div id='logoBox'>
+            <FaSquareTwitter id='logoTwitter'/>
+            <FaFacebook id='logoFb'/>
+            <FaGooglePlus id='logoGoogle'/>
+          </div>
+          <label for='inputEmail'>Email</label>
+          <br />
+          <input 
+          type='email' 
+          id='inputEmail'
+          value={email} // Bind value to state for controlled input
+          onChange={(event) => setEmail(event.target.value)}>
+          </input>
+          <br />
+          <label for='inputPassword'>Password</label>
+          <br />
+          <input 
+          type='password' 
+          id='inputPassword'
+          value={password} 
+          onChange={(event) => setPassword(event.target.value)}>
+          </input>
+          <br />
+          <div id='btnLoginBox'>
+            <button id='btnLogin' onClick={() => {console.log('Email = ' + email + '\n' + 'Password = ' + password); setEmail(''); setPassword('');}}>Login</button>
+          </div>
+          <br />
+          <div id='signUpBox'>
+            Don't have an account? <a href='/signup' id='signUpLink'>Sign Up</a>
+          </div>
+      </div>
     </div>
   )
 }
